a1492e22891418e1e94524143e7f8a0016da4a4c
maricotagc <maricotagc@gmail.com>
2021-09-24 19:17:57 +0200

    book id auto generated and fix for junit

---
+++ src/test/java/mm/example/Block7/repository/AbstractBaseTest.java
@@ -0,0 +1,46 @@
+package mm.example.Block7.repository;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public abstract class AbstractBaseTest {
+
+    public void createTables(Connection connection) throws SQLException {
+        Statement statement = null;
+
+        try {
+            statement = connection.createStatement();
+            String createBookTable = "DROP TABLE IF EXISTS book;" +
+                    " CREATE TABLE book " +
+                    "(id INTEGER PRIMARY KEY AUTOINCREMENT," +
+                    " name TEXT NOT NULL, " +
+                    " author TEXT NOT NULL)";
+            statement.executeUpdate(createBookTable);
+
+            String createLibraryTable = "DROP TABLE IF EXISTS library;" +
+                    "CREATE TABLE library " +
+                    "(id INTEGER PRIMARY KEY AUTOINCREMENT," +
+                    " name TEXT NOT NULL, " +
+                    " address VARCHAR(500) NOT NULL)";
+            statement.executeUpdate(createLibraryTable);
+
+            String createBookLibraryTable = "DROP TABLE IF EXISTS book_library;" +
+                    "CREATE TABLE book_library " +
+                    "(book_id INT NOT NULL," +
+                    " library_id INT NOT NULL, " +
+                    " total_copies INT NOT NULL, " +
+                    " available_copies INT NOT NULL, " +
+                    " FOREIGN KEY(book_id) REFERENCES book(id)," +
+                    " FOREIGN KEY(library_id) REFERENCES library(id) )";
+            statement.executeUpdate(createBookLibraryTable);
+
+        } catch (Exception e) {
+            System.err.println(e.getClass().getName() + ": " + e.getMessage());
+            System.exit(0);
+        } finally {
+            statement.close();
+        }
+    }
+
+}
--- src/test/java/mm/example/Block7/repository/BookRepositoryTest.java
+++ src/test/java/mm/example/Block7/repository/BookRepositoryTest.java
@@ -6,20 +6,12 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
-import java.sql.Connection;
-import java.sql.SQLException;
-import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-public class BookRepositoryTest {
-
-    @Test
-    public void shouldConnectToTheDatabase() throws Exception {
-        DatabaseManager databaseManager = new DatabaseManager();
-    }
+public class BookRepositoryTest extends AbstractBaseTest{
 
     @Before
     public void setUp() throws Exception {
@@ -34,21 +26,18 @@ public class BookRepositoryTest {
     @Test
     public void shouldReturn1ForBookSuccessfullyAdded() throws Exception {
         DatabaseManager databaseManager = new DatabaseManager();
+        BookRepository bookRepository = new BookRepository(databaseManager.getConnection());
 
         Book book1 = new Book();
-        book1.setId(1);
         book1.setName("Harry Potter and the Philosophers Stone");
         book1.setAuthor("J.K. Rowling");
 
-        BookRepository bookRepository = new BookRepository(databaseManager.getConnection());
-
         try {
-            int result = bookRepository.add(book1);
-            Book book = bookRepository.findById(1);
-            Assert.assertEquals(1, result);
-            Assert.assertEquals("Harry Potter and the Philosophers Stone", book.getName());
-            Assert.assertEquals("J.K. Rowling", book.getAuthor());
-            Assert.assertEquals(1, book.getId());
+            bookRepository.add(book1);
+            Book actualBook = bookRepository.findById(1);
+            Assert.assertEquals("Harry Potter and the Philosophers Stone", actualBook.getName());
+            Assert.assertEquals("J.K. Rowling", actualBook.getAuthor());
+            Assert.assertEquals(1, actualBook.getId());
         } finally {
             databaseManager.closeConnection();
         }
@@ -59,15 +48,13 @@ public class BookRepositoryTest {
         DatabaseManager databaseManager = new DatabaseManager();
         BookRepository bookRepository = new BookRepository(databaseManager.getConnection());
         Book book1 = new Book();
-        book1.setId(1);
         book1.setName("Harry Potter and the Philosophers Stone");
         book1.setAuthor("J.K. Rowling");
         try {
-
-
             bookRepository.add(book1);
-            int result = bookRepository.remove(book1.getId());
-            Assert.assertEquals(1, result);
+            Assert.assertEquals("Harry Potter and the Philosophers Stone", bookRepository.findById(1).getName());
+            Assert.assertEquals("Book was successfully removed from book table.", bookRepository.remove(1));
+            Assert.assertEquals(null, bookRepository.findById(1).getName());
         } finally {
             databaseManager.closeConnection();
         }
@@ -76,10 +63,7 @@ public class BookRepositoryTest {
     @Test
     public void viewAllBooks() throws Exception {
         DatabaseManager databaseManager = new DatabaseManager();
-        BookRepository bookRepository =
-
-
-                new BookRepository(databaseManager.getConnection());
+        BookRepository bookRepository = new BookRepository(databaseManager.getConnection());
 
         try {
             Book book1 = new Book();
@@ -100,6 +84,7 @@ public class BookRepositoryTest {
 
             List<Book> actual = bookRepository.findAll();
             assertThat(expected).hasSameElementsAs(actual);
+
         } finally {
             databaseManager.closeConnection();
         }
@@ -109,58 +94,19 @@ public class BookRepositoryTest {
     public void findBookById() throws Exception {
         DatabaseManager databaseManager = new DatabaseManager();
         BookRepository bookRepository = new BookRepository(databaseManager.getConnection());
+
         try {
             Book book1 = new Book();
-            book1.setId(6);
+            book1.setId(1);
             book1.setName("Harry Potter and the Philosophers Stone");
             book1.setAuthor("J.K. Rowling");
-            bookRepository.add(book1
+            bookRepository.add(book1);
 
-
-            );
-
-            Book book = bookRepository.findById(6);
-            Assert.assertEquals(6, book.getId());
+            Book book = bookRepository.findById(1);
+            Assert.assertEquals(1, book.getId());
         } finally {
             databaseManager.closeConnection();
         }
     }
 
-    public void createTables(Connection connection) throws SQLException {
-        Statement statement = null;
-
-        try {
-            statement = connection.createStatement();
-            String createBookTable = "DROP TABLE IF EXISTS book;" +
-                    " CREATE TABLE book " +
-                    "(id INT PRIMARY KEY NOT NULL," +
-                    " name TEXT NOT NULL, " +
-                    " author TEXT NOT NULL)";
-            statement.executeUpdate(createBookTable);
-
-            String createLibraryTable = "DROP TABLE IF EXISTS library;" +
-                    "CREATE TABLE library " +
-                    "(id INT PRIMARY KEY NOT NULL," +
-                    " name TEXT NOT NULL, " +
-                    " address VARCHAR(500) NOT NULL)";
-            statement.executeUpdate(createLibraryTable);
-
-            String createBookLibraryTable = "DROP TABLE IF EXISTS book_library;" +
-                    "CREATE TABLE book_library " +
-                    "(book_id INT NOT NULL," +
-                    " library_id INT NOT NULL, " +
-                    " total_copies INT NOT NULL, " +
-                    " available_copies INT NOT NULL, " +
-                    " FOREIGN KEY(book_id) REFERENCES book(id)," +
-                    " FOREIGN KEY(library_id) REFERENCES library(id) )";
-            statement.executeUpdate(createBookLibraryTable);
-
-        } catch (Exception e) {
-            System.err.println(e.getClass().getName() + ": " + e.getMessage());
-            System.exit(0);
-        } finally {
-            statement.close();
-        }
-        System.out.println("Tables created successfully");
-    }
 }
